448dcfea83224785817836fda6954c7c80c2ef8e
Almaz192 <kombiklay156@gmail.com>
2024-11-19 12:53:07 +0600

    created backend reauest

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
 import { ProductCard } from "./components/ProductCard";
-
-
-const products=[
-  {
-    id:0,
-    name :"Product 1",
-    category : "Category 1" ,
-    price : 200,
-    img: "https://samsungstore.kg/files/media/14/14630.png"
-  },
-  {
-    id:1,
-    name :"Product 2",
-    category : "Category 2" ,
-    price : 400,
-    img: "https://samsungstore.kg/files/media/14/14630.png"
-  },
-]
-
-
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [loading,setLoading]=useState([true]) 
+  const [products,setProducts]=useState([]) 
+
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+            .finally(()=> setLoading(false)) 
+  },[])
+
   return (
-    <div>{
+    <div>
+    {loading && <div>loading..</div>}
+    {
     products.map((product) => (<ProductCard data={product}/>))
-  }
+}
     </div>
   );
 }
